ffdeaf5291d848db7880c39bf96d0c0d4ae60c8a
Michael Grieshaber <MichaelJGrieshaber@gmail.com>
2021-10-08 09:03:19 -0500

    models done

---
+++ models/user.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const User = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    password_digest: { type: String, required: true, select: false },
+  },
+  { timestamps: true }
+);
+export default mongoose.model("users", User);
