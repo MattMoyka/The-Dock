6ec10d5373ecaaf5b90fb905b19bf0d0e6469beb
mattmoyka <mmoyka17@gmail.com>
2021-10-08 15:20:06 -0400

    app.js complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import { Route } from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom'
 import './App.css';
+import { useState, useEffect } from 'react'
+import { verifyUser } from './services/users'
 import Landing from './screens/Landing/Landing'
 import SignUp from './screens/Signin/Signin'
 import Signin from './screens/Signin/Signin';
@@ -10,31 +12,41 @@ import EditItem from './screens/EditItem/EditItem';
 import Nav from './components/Nav/Nav';
 
 function App() {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await verifyUser()
+      user ? setUser(user) : setUser(null)
+    }
+    fetchUser()
+  }, [])
+
   return (
     <div className="App">
       <header className="header">
         <Nav />
       </header>
       <Route exact path="/">
-        <Landing />
+        <Landing user={user} />
       </Route>
       <Route exact path="/signup">
-        <SignUp />
+        <SignUp setUser={setUser} />
       </Route>
       <Route exact path="/signin">
-        <Signin />
+        <Signin setUser={setUser} />
       </Route>
       <Route exact path="/items">
-        <Listing />
+        <Listing user={user} />
       </Route>
       <Route exact path="/items/:id">
-        <Details />
+        <Details user={user} />
       </Route>
       <Route exact path="/new">
-        <AddItem />
+        {user ? <AddItem user={user} /> : <Redirect to='/signup' />}
       </Route>
       <Route exact path="/edit/:id">
-        <EditItem />
+        {user ? <EditItem user={user} /> : <Redirect to='/signup' />}
       </Route>
     </div>
   );
