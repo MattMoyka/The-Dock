e8e26121b23cc3647fcd5a74d14bd9f2e0bc973d
mattmoyka <mmoyka17@gmail.com>
2021-10-08 15:35:03 -0400

    finished listing

--- client/src/screens/Listing/Listing.jsx
+++ client/src/screens/Listing/Listing.jsx
@@ -1,7 +1,37 @@
-export default function Listing() {
-  return (
-    <div>
+import { useState, useEffect } from 'react'
+import { getItems } from '../../services/items'
+import Layout from '../../components/Layout/Layout'
 
-    </div>
+
+export default function Listing() {
+
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const allItems = await getItems()
+      setItems(allItems)
+    }
+    fetchItems()
+  }, [])
+
+  return (
+    <Layout>
+      <div>
+        {items.map(item => {
+          return (
+            <div key={item?.title}>
+              <h1>{item?.title}</h1>
+              <img src={item?.imgURL} alt={item?.title} />
+              <h2>{item?.description}</h2>
+              <div>
+                <p>{item?.location}</p>
+                <p>{item?.price}</p>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </Layout>
   )
 }
