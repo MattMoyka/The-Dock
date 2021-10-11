03d86c114a9acfbb814d998d231a3f5bbc6bf849
Michael Grieshaber <MichaelJGrieshaber@gmail.com>
2021-10-11 15:39:15 -0500

    additem css

--- client/src/screens/AddItem/AddItem.jsx
+++ client/src/screens/AddItem/AddItem.jsx
@@ -1,89 +1,97 @@
-import Layout from '../../components/Layout/Layout'
-import { createItem } from "../../Services/items"
-import { useState } from "react"
-import { Redirect } from 'react-router-dom'
+import Layout from "../../components/Layout/Layout";
+import { createItem } from "../../Services/items";
+import { useState } from "react";
+import { Redirect } from "react-router-dom";
+import "./AddItem.css";
 
 export default function AddItem(props) {
   const [item, setItem] = useState({
-    title: '', category: '', price: '', imgURL: '', description: '', location: ''
-  })
-  const [isCreated, setCreated] = useState(false)
+    title: "",
+    category: "",
+    price: "",
+    imgURL: "",
+    description: "",
+    location: "",
+  });
+  const [isCreated, setCreated] = useState(false);
 
   const handleChange = (event) => {
-    const { name, value } = event.target
+    const { name, value } = event.target;
     setItem({
       ...item,
       [name]: value,
-    })
-  }
+    });
+  };
   const handleSubmit = async (event) => {
-    event.preventDefault()
-    const created = await createItem(item)
-    setCreated({ created })
-  }
+    event.preventDefault();
+    const created = await createItem(item);
+    setCreated({ created });
+  };
   if (isCreated) {
-    return <Redirect to={`/items`} />
+    return <Redirect to={`/items`} />;
   }
 
   return (
     <div>
       <Layout user={props.user}>
-        <form className='create-form' onSubmit={handleSubmit}>
-          <input
-            className='input-name'
-            placeholder='Title'
-            value={item.title}
-            name='title'
-            required
-            autoFocus
-            onChange={handleChange}
-          />
+        <div className="addItem">
+          <form className="create-form" onSubmit={handleSubmit}>
+            <input
+              className="input-name"
+              placeholder="Title"
+              value={item.title}
+              name="title"
+              required
+              autoFocus
+              onChange={handleChange}
+            />
 
-          <input
-            className='input-category'
-            placeholder='Category'
-            value={item.category}
-            name='category'
-            required
-            onChange={handleChange}
-          />
-          <input
-            className='input-price'
-            placeholder='Price'
-            value={item.price}
-            name='price'
-            required
-            onChange={handleChange}
-          />
-          <input
-            className='input-location'
-            placeholder='Location'
-            value={item.location}
-            name='location'
-            required
-            onChange={handleChange}
-          />
-          <input
-            className='input-imgURL'
-            placeholder='Image URL'
-            value={item.imgURL}
-            name='imgURL'
-            required
-            onChange={handleChange}
-          />
-          <textarea
-            className='input-description'
-            placeholder='Description'
-            value={item.description}
-            name='description'
-            required
-            onChange={handleChange}
-          />
-          <button type='submit' className='submit-button'>
-            Submit
-          </button>
-        </form>
+            <input
+              className="input-category"
+              placeholder="Category"
+              value={item.category}
+              name="category"
+              required
+              onChange={handleChange}
+            />
+            <input
+              className="input-price"
+              placeholder="Price"
+              value={item.price}
+              name="price"
+              required
+              onChange={handleChange}
+            />
+            <input
+              className="input-location"
+              placeholder="Location"
+              value={item.location}
+              name="location"
+              required
+              onChange={handleChange}
+            />
+            <input
+              className="input-imgURL"
+              placeholder="Image URL"
+              value={item.imgURL}
+              name="imgURL"
+              required
+              onChange={handleChange}
+            />
+            <textarea
+              className="input-description"
+              placeholder="Description"
+              value={item.description}
+              name="description"
+              required
+              onChange={handleChange}
+            />
+            <button type="submit" className="submit-button">
+              Submit
+            </button>
+          </form>
+        </div>
       </Layout>
     </div>
-  )
+  );
 }
