d9736b9aedf1cfb37e52c157e78b3b0374345b8f
Mirian Zhagui <mirianzhagui@Mirians-MacBook-Air.local>
2021-10-13 16:06:37 -0400

    updated my css for details

--- client/src/screens/Details/Details.jsx
+++ client/src/screens/Details/Details.jsx
@@ -22,19 +22,24 @@ export default function Details(props) {
       <Layout user={props.user}>
       </Layout>
       <div className="detailsparent-div">
-      <div className="details-image">
-          <img className="image-detail" src={item?.imgURL} alt={item?.title} />
-      </div>
-      <div className="info-div">
+        <div className="info-div">
+        <div className="top-detail">
+        <p id="location-detail" className="fas fa-map-marker-alt">{item?.location}</p>
+        </div>
           <h1 className="h1-details">{item?.title}</h1>
-          <h3 id="location-detail" className="fas fa-map-marker-alt">{item?.location}</h3>
+         <p className="price-detail"> ${item?.price}/HR</p>
           <div className="description-div">
           <p className="description-tag">Item Description </p>
-            <p>{item?.description}</p>
-            <button className="rent-button" onClick={() => alert('congrats on the purchase')}>${item?.price}/HR-Rent Now</button>
-       </div>
+            <p className="desc-details">{item?.description}</p>
+          </div>
+   <div className="the-buttons">
+          <button className="rent-button" onClick={() => alert('congrats on the purchase')}>RENT NOW</button>
           <Link  className="far fa-edit fa-2x" to={`/edit/${item?._id}`}></Link>
-      </div> 
+     </div>
+        </div>
+        <div className="details-image">
+          <img className="image-detail" src={item?.imgURL} alt={item?.title} />
+      </div>
       </div>
       </div>
    
