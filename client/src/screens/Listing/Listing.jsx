aec4f91e6815ad2272d2babd52a746cd70cf3064
mattmoyka <mmoyka17@gmail.com>
2021-10-13 11:02:49 -0400

    listing page css fixes

--- client/src/screens/Listing/Listing.jsx
+++ client/src/screens/Listing/Listing.jsx
@@ -24,18 +24,18 @@ export default function Listing(props) {
         {items.map(item => {
           return (
             <div key={item?.title} className="itemCards">
-              <Link to={`/items/${item?._id}`} style={{ textDecoration: 'none', color:'black' }}>
-              <div className="item-title">
-              </div>
-              <img src={item?.imgURL} alt={item?.title} />
-              <div className="img-desc">
-              <h2>{item?.title}</h2>
-              {/* <p>"{item?.description}"</p> */}
-              <div id="loc-details">
-                <h3 id="hoverShow1" className="fas fa-map-marker-alt"> {item?.location} | $ {item?.price}</h3>
-                {/* <p id="hoverShow1"> $ {item?.price}</p> */}
-              </div>
-              </div>
+              <Link to={`/items/${item?._id}`} style={{ textDecoration: 'none', color: 'black' }}>
+                <div className="item-title">
+                </div>
+                <img src={item?.imgURL} className='listing-img' alt={item?.title} />
+                <div className="img-desc">
+                  <h2 className='title-text'>{item?.title}</h2>
+                  {/* <p>"{item?.description}"</p> */}
+                  <div id="loc-details">
+                    <h3 id="hoverShow1" className="fas fa-map-marker-alt"> {item?.location} | $ {item?.price}</h3>
+                    {/* <p id="hoverShow1"> $ {item?.price}</p> */}
+                  </div>
+                </div>
               </Link>
             </div>
           )
