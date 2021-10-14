81768cdd44d6db01f3be45f73b279a520cf2151f
Michael Grieshaber <MichaelJGrieshaber@gmail.com>
2021-10-14 11:32:07 -0500

    footer

--- client/src/screens/Details/Details.jsx
+++ client/src/screens/Details/Details.jsx
@@ -1,47 +1,54 @@
 import Layout from "../../components/Layout/Layout";
-import { useParams, Link } from 'react-router-dom'
-import { useState, useEffect } from 'react'
-import { getItem } from '../../Services/items'
-import './Details.css'
-
+import { useParams, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { getItem } from "../../Services/items";
+import "./Details.css";
 
 export default function Details(props) {
-  const { id } = useParams()
-  const [item, setItem] = useState(null)
+  const { id } = useParams();
+  const [item, setItem] = useState(null);
 
   useEffect(() => {
     const fetchItem = async () => {
-      const item = await getItem(id)
-      setItem(item)
-    }
-    fetchItem()
-  }, [id])
+      const item = await getItem(id);
+      setItem(item);
+    };
+    fetchItem();
+  }, [id]);
 
   return (
-    <div >
-      <Layout user={props.user}>
-      </Layout>
+    <div>
+      <Layout user={props.user}></Layout>
       <div className="detailsparent-div">
         <div className="info-div">
-        <div className="top-detail">
-        <p id="location-detail" className="fas fa-map-marker-alt">{item?.location}</p>
-        </div>
+          <div className="top-detail">
+            <p id="location-detail" className="fas fa-map-marker-alt">
+              {item?.location}
+            </p>
+          </div>
           <h1 className="h1-details">{item?.title}</h1>
-         <p className="price-detail"> ${item?.price}/DAY</p>
+          <p className="price-detail"> ${item?.price}/DAY</p>
           <div className="description-div">
-          <p className="description-tag">Item Description </p>
+            <p className="description-tag">Item Description </p>
             <p className="desc-details">{item?.description}</p>
           </div>
-   <div className="the-buttons">
-          <button className="rent-button" onClick={() => alert('congrats on the purchase')}>RENT NOW</button>
-          <Link  className="far fa-edit fa-2x" to={`/edit/${item?._id}`}></Link>
-     </div>
+          <div className="the-buttons">
+            <button
+              className="rent-button"
+              onClick={() => alert("congrats on the purchase")}
+            >
+              RENT NOW
+            </button>
+            <Link
+              className="far fa-edit fa-2x"
+              to={`/edit/${item?._id}`}
+            ></Link>
+          </div>
         </div>
         <div className="details-image">
           <img className="image-detail" src={item?.imgURL} alt={item?.title} />
+        </div>
       </div>
-      </div>
-      </div>
-   
-  )
+    </div>
+  );
 }
