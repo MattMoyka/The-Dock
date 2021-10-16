5b7eb122acbb37dcffd4a296b7afa07d5e9bc86a
mattmoyka <mmoyka17@gmail.com>
2021-10-16 09:28:36 -0400

    desktop landing screen made products animation show up on screen without scrolling

--- client/src/components/Generator/RandomPhotos.jsx
+++ client/src/components/Generator/RandomPhotos.jsx
@@ -3,71 +3,71 @@ import { getItems } from '../../Services/items'
 import { Link } from 'react-router-dom'
 
 export default function RandomPhotos() {
-    const [items, setItems] = useState([])
-    const [one, setOne] = useState("");
-    const [two, setTwo] = useState("");
-    const [three, setThree] = useState("");
-    const [four, setFour] = useState("")
+  const [items, setItems] = useState([])
+  const [one, setOne] = useState('1');
+  const [two, setTwo] = useState('2');
+  const [three, setThree] = useState('3');
+  const [four, setFour] = useState("")
 
-    useEffect(() => {
-        const fetchItems = async () => {
-          const allItems = await getItems()
-          setItems(allItems)
-        }
-        fetchItems()
-      }, [])
+  useEffect(() => {
+    const fetchItems = async () => {
+      const allItems = await getItems()
+      setItems(allItems)
+    }
+    fetchItems()
+  }, [])
 
-      console.log(one)
+  console.log(one)
 
-    const RandomNumber = () => {
-        
-        // add side effect to component
-        // useEffect(() => {
-          // create interval
-        //   const interval = setInterval(
-            // set number every 5s
-                let varOne = (Math.floor(Math.random() * items.length/3));
-                let varTwo = (Math.floor(Math.random() * (items.length * 2 / 3 - items.length / 3) + (items.length / 3)));
-                let varThree = (Math.floor(Math.random() * (items.length - items.length * 2 / 3) + (items.length * 2 / 3)));
-                let varFour = (Math.random() * 2 / 2)
+  const RandomNumber = () => {
 
-
-                setOne(varOne !== one ? varOne : varOne + 1);
-                setTwo(varTwo !== two ? varTwo : varTwo + 1);
-                setThree(varThree !== three ? varThree : varThree - 1);
-                setFour(varFour);
+    // add side effect to component
+    // useEffect(() => {
+    // create interval
+    //   const interval = setInterval(
+    // set number every 5s
+    let varOne = (Math.floor(Math.random() * items.length / 3));
+    let varTwo = (Math.floor(Math.random() * (items.length * 2 / 3 - items.length / 3) + (items.length / 3)));
+    let varThree = (Math.floor(Math.random() * (items.length - items.length * 2 / 3) + (items.length * 2 / 3)));
+    let varFour = (Math.random() * 2 / 2)
 
 
-          // clean up interval on unmount
-        // }, []);
-      
-      };
-      useEffect(()=> {  
-          setTimeout(RandomNumber, 6000)
-      }, [four]);
+    setOne(varOne !== one ? varOne : varOne + 1);
+    setTwo(varTwo !== two ? varTwo : varTwo + 1);
+    setThree(varThree !== three ? varThree : varThree - 1);
+    setFour(varFour);
 
-      console.log(one)
-      console.log(two)
 
-    return (
-        <>
-            <div className="Rendered"> 
-                <div className="block">
-                    <Link to={`/items/${items[one]?._id}`}>
-                    <img src={items[one]?.imgURL} id="aniPic" />
-                    </Link>
-                </div>
-                <div className="block">
-                    <Link to={`/items/${items[two]?._id}`}>
-                    <img src={items[two]?.imgURL} id="aniPic" />
-                    </Link>
-                </div>
-                <div className="block">
-                    <Link to={`/items/${items[three]?._id}`}>
-                    <img src={items[three]?.imgURL} id="aniPic" />
-                    </Link>
-                </div>
-            </div>
-        </>
-    )
+    // clean up interval on unmount
+    // }, []);
+
+  };
+  useEffect(() => {
+    setTimeout(RandomNumber, 6000)
+  }, [four]);
+
+  console.log(one)
+  console.log(two)
+
+  return (
+    <>
+      <div className="Rendered">
+        <div className="block">
+          <Link to={`/items/${items[one]?._id}`}>
+            <img src={items[one]?.imgURL} id="aniPic" />
+          </Link>
+        </div>
+        <div className="block">
+          <Link to={`/items/${items[two]?._id}`}>
+            <img src={items[two]?.imgURL} id="aniPic" />
+          </Link>
+        </div>
+        <div className="block">
+          <Link to={`/items/${items[three]?._id}`}>
+            <img src={items[three]?.imgURL} id="aniPic" />
+          </Link>
+        </div>
+      </div>
+    </>
+  )
 }
